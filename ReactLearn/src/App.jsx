5b1b88bdd93393855720f1fc4ea647694a15484e
Harsh sahu <sahuharsh164@gmail.com>
2024-01-11 16:30:39 +0530

    3

--- ReactLearn/src/App.jsx
+++ ReactLearn/src/App.jsx
@@ -1,13 +1,20 @@
- 
-
- 
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-   
-
+  const [color, setColor] = useState("olive");
   return (
-   <h1>React Learning</h1>
-  )
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center shadow-lg gap-3 bg-white py-2 rounded-3xl">
+          <button className="outline-none px-4 bg-red-600"> Red</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
